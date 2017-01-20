6d01d82a70b44aa3f21ff48dcbfffcc2a345e970
Vokiman <sim.nowak@gmx.at>
2017-01-20 11:11:44 +0100

    Is scheiß egal

--- app/src/main/java/com/hgbjg14/cahproject/BlackCardActivity.java
+++ app/src/main/java/com/hgbjg14/cahproject/BlackCardActivity.java
@@ -21,8 +21,7 @@ import java.util.List;
 public class BlackCardActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "MyActivity";
 
-    private List<BlackCard> blackCards = new LinkedList<>();
-    private List<String> whiteCards = new LinkedList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,76 +37,5 @@ public class BlackCardActivity extends AppCompatActivity implements View.OnClick
         //View cardView = vi.inflate(R.layout.fragment_card, R.id.cardView, false);
     }
 
-    private class LoadCards extends AsyncTask<String, Void, Void> {
 
-        @Override
-        protected Void doInBackground(String... params) {
-            blackCards.clear();
-            whiteCards.clear();
-            for (String param : params) {
-                try {
-
-                    int tempId = getResources().getIdentifier(param, "raw", getPackageName());
-                    String jsonString = GetStringFromRawResource(tempId);
-
-                    JSONObject reader = new JSONObject(jsonString);
-                    JSONArray blackCardsArray = reader.getJSONArray("blackCards");
-                    JSONArray whiteCardsArray = reader.getJSONArray("whiteCards");
-
-                    JSONObject currentObject = null;
-                    int temp = 0;
-                    for (int i = 0; i < blackCardsArray.length(); i++) {
-                        currentObject = blackCardsArray.getJSONObject(i);
-                        blackCards.add(new BlackCard(currentObject.getString("text"), currentObject.getInt("pick")));
-
-                        temp = i;
-                    }
-                    Log.d(TAG, Integer.toString(temp));
-                    Log.d(TAG, Integer.toString(blackCards.size()));
-                    for (int i = 0; i < whiteCardsArray.length(); i ++) {
-                        whiteCards.add(whiteCardsArray.getString(i));
-                        temp = i;
-                    }
-                    Log.d(TAG, Integer.toString(temp));
-                    Log.d(TAG, Integer.toString(whiteCards.size()));
-
-                }
-                catch (Exception e) {
-                    Log.d(TAG, e.getMessage());
-                }
-            }
-
-            return null;
-        }
-    }
-
-    private String GetStringFromRawResource(int id) {
-        String retString = "";
-        try {
-            InputStream inStream = getResources().openRawResource(id);
-            retString = StreamToString(inStream);
-            inStream.close();
-        }
-        catch (Exception e) {
-
-        }
-
-        return retString;
-    }
-
-    private String StreamToString(InputStream inStream) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            reader.close();
-        }
-        catch (Exception e) {
-
-        }
-        return sb.toString();
-    }
 }
--- app/src/main/java/com/hgbjg14/cahproject/MainActivity.java
+++ app/src/main/java/com/hgbjg14/cahproject/MainActivity.java
@@ -14,15 +14,20 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MyActivity";
 
     private String inString = null;
-
+    private ArrayList<BlackCard> blackCards = new ArrayList<BlackCard>();
+    private ArrayList<String> whiteCards = new ArrayList<String>();
+    private Integer currentWhiteCardCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +37,87 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+
+        new LoadCards().execute("base_set");
     }
 
     @Override
     public void onClick(View v) {
-        Log.d(TAG, "onClick: ");
-        String base = "base_set";
-        String exp1 = "expansion_1";
-        Intent intent = new Intent(this, BlackCardActivity.class);
-        startActivity(intent);
-
-
+        Log.d(TAG, whiteCards.get(currentWhiteCardCounter));
+        currentWhiteCardCounter = (currentWhiteCardCounter + 1) % whiteCards.size();
     }
 
 
+    private class LoadCards extends AsyncTask<String, Void, Void> {
 
+        @Override
+        protected Void doInBackground(String... params) {
+            blackCards.clear();
+            whiteCards.clear();
+            for (String param : params) {
+                try {
+
+                    int tempId = getResources().getIdentifier(param, "raw", getPackageName());
+                    String jsonString = GetStringFromRawResource(tempId);
+
+                    JSONObject reader = new JSONObject(jsonString);
+                    JSONArray blackCardsArray = reader.getJSONArray("blackCards");
+                    JSONArray whiteCardsArray = reader.getJSONArray("whiteCards");
+
+                    JSONObject currentObject = null;
+                    int temp = 0;
+                    for (int i = 0; i < blackCardsArray.length(); i++) {
+                        currentObject = blackCardsArray.getJSONObject(i);
+                        blackCards.add(new BlackCard(currentObject.getString("text"), currentObject.getInt("pick")));
+
+                        temp = i;
+                    }
+                    for (int i = 0; i < whiteCardsArray.length(); i ++) {
+                        whiteCards.add(whiteCardsArray.getString(i));
+                        temp = i;
+                    }
+
+                }
+                catch (Exception e) {
+                    Log.d(TAG, e.getMessage());
+                }
+            }
+            long seed = System.nanoTime();
+            Collections.shuffle(whiteCards, new Random(seed));
+            Collections.shuffle(blackCards, new Random(seed));
+
+            return null;
+        }
+    }
+
+    private String GetStringFromRawResource(int id) {
+        String retString = "";
+        try {
+            InputStream inStream = getResources().openRawResource(id);
+            retString = StreamToString(inStream);
+            inStream.close();
+        }
+        catch (Exception e) {
+
+        }
+
+        return retString;
+    }
+
+    private String StreamToString(InputStream inStream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            reader.close();
+        }
+        catch (Exception e) {
+
+        }
+        return sb.toString();
+    }
 
 }
