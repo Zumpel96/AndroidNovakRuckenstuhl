337d6f91239a961830f49ffeb034c2ce2206d3ad
Vokiman <sim.nowak@gmx.at>
2017-01-25 18:37:20 +0100

    Nobody cares

---
+++ app/src/main/java/com/hgbjg14/cahproject/CaHBroadcastReceiver.java
@@ -0,0 +1,86 @@
+package com.hgbjg14.cahproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CaHBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager manager;
+    private Channel channel;
+    private MainActivity activity;
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+/*
+    private PeerListListener peerListListener = new PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+            List<WifiP2pDevice> refreshedPeers = new ArrayList<>(peerList.getDeviceList());
+
+            if (!refreshedPeers.equals(peers)) {
+                peers.clear();
+                peers.addAll(refreshedPeers);
+
+                // If an AdapterView is backed by this data, notify it
+                // of the change.  For instance, if you have a ListView of
+                // available peers, trigger an update.
+                //((WiFiPeerListAdapter) getListAdapter()).notifyDataSetChanged();
+
+                // Perform any other updates needed based on the new list of
+                // peers connected to the Wi-Fi P2P network.
+            }
+
+            if (peers.size() == 0) {
+                Log.d(MainActivity.TAG, "No devices found");
+                return;
+            }
+        }
+    }*/
+
+    public CaHBroadcastReceiver(WifiP2pManager manager, Channel channel, MainActivity activity) {
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                activity.setIsWifiP2pEnabled(true);
+            } else {
+                activity.setIsWifiP2pEnabled(false);
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            if(this.manager != null) {
+                this.manager.requestPeers(this.channel, (PeerListListener)this.activity.getFragmentManager().findFragmentById(R.layout.fragment_item_list));
+            }
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+            // Connection state changed!  We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            /*DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+                    .findFragmentById(R.id.frag_list);
+            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));*/
+
+        }
+    }
+}
