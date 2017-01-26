284b2e5deeed49ced1fef51462b788b6807d6626
Zumpel96 <bernhardruckenstuhl@gmx.at>
2017-01-26 20:21:11 +0100

    gaga

--- app/src/main/java/com/hgbjg14/cahproject/AboutActivity.java
+++ app/src/main/java/com/hgbjg14/cahproject/AboutActivity.java
@@ -1,24 +1,32 @@
 package com.hgbjg14.cahproject;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-public class AboutActivity extends AppCompatActivity implements View.OnClickListener  {
+public class AboutActivity extends Fragment implements View.OnClickListener  {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentActivity faActivity = (FragmentActivity)super.getActivity();
+
+        RelativeLayout rlLayout = (RelativeLayout)inflater.inflate(R.layout.activity_about, container, false);
 
         Button button = null;
-        button = (Button) findViewById(R.id.about_back_button);
+        button = (Button)rlLayout.findViewById(R.id.about_back_button);
         button.setOnClickListener(this);
+        return rlLayout;
     }
 
     @Override
     public void onClick(View v) {
-        finish();
+
     }
 }
